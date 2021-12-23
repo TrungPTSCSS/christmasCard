c4adbcb32195695b5f085172c3e80419dab077d9
Trung Phạm <TrungPham@users.noreply.github.com>
2021-12-24 05:16:58 +0700

    app

--- christmasTheme/src/app/app-routing.module.ts
+++ christmasTheme/src/app/app-routing.module.ts
@@ -4,13 +4,22 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+    loadChildren: () =>
+      import('./pages/home/home.module').then((m) => m.HomePageModule),
+    pathMatch: 'full',
+  },
+  {
+    path: 'chistmas',
+    loadChildren: () =>
+      import('./pages/chistmas/chistmas.module').then(
+        (m) => m.ChistmasPageModule
+      ),
+  },
 ];
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
